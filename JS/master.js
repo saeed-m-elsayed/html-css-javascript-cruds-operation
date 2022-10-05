2454d608333287e99c2d1d2870f5e735a7466661
saeed-m-elsayed <isaeedeagle@gmail.com>
2022-10-06 01:36:49 +0200

    search by title added search by category added clean data added update button added

--- JS/master.js
+++ JS/master.js
@@ -17,8 +17,11 @@ let mainCss = document.getElementById("main-css")
 let morning = document.getElementById("morning")
 let night = document.getElementById("night")
 let up = document.getElementById("up")
+submitButton = 'create'
 let totalPrice
 let i = 0;
+let tmp;
+
 
 // localStorage.clear()
 let mode = function (mode) {
@@ -88,8 +91,36 @@ if(localStorage.product != null){
 }
 
 
+function cleanData(){
+    if(category.value == ''){
+        category.focus()
+        category.placeholder = 'You Must Enter Category !!'
+    }
+    if(count.value > 100){
+        count.value = ''
+        count.placeholder = 'Count Must Be 100 or Less'
+        count.focus()
+    }
+    if(ads.value == '' || ads.value < 0)
+    ads.value = 0;
+    if(discount.value == '' || discount.value < 0)
+    discount.value = 0
+    if(taxes.value == '' || taxes.value < 0)
+    taxes.value = 0
+    if(price.value == ''){
+        price.focus()
+        price.placeholder = 'You Must Enter price !!'
+    }
+    if(title.value == ''){
+        title.focus()
+        title.placeholder = 'You Must Enter Title !!'
+    }
+    getPrice()
+}
+
 submit.onclick = function(){
-    if(price.value.length != 0 && title.value.length != 0 && category.value.length != 0){
+    cleanData()
+    if(price.value.length != 0 && title.value.length != 0 && category.value.length != 0  && count.value <= 100){
     let newProduct = {
         title:title.value,
         price:price.value,
@@ -100,18 +131,31 @@ submit.onclick = function(){
         count:count.value,
         category:category.value
     }
+
+    if(submitButton == 'create'){
     if (newProduct.count > 1){
         for(let i= 0 ; i < newProduct.count ; i++)
     dataProduct.push(newProduct)
     }else dataProduct.push(newProduct)
+    }else{
+        dataProduct[tmp] = newProduct
+        submit.innerHTML = 'CREATE'
+        count.style.display = 'block'
+        submitButton = 'create'
+    }
+
+
+
     localStorage.setItem("product", JSON.stringify(dataProduct))
-    // console.log(dataProduct)
     clearData()
     readData()
     deleteButton()
 }
 }
 // console.log(JSON.parse(localStorage.product))
+
+
+
 
 // start clear inputs
 
@@ -144,7 +188,7 @@ function readData(){
     <td>${dataProduct[i].discount}</td>
     <td>${dataProduct[i].category}</td>
     <td>${dataProduct[i].total}</td>
-    <td><button id="update-item">update</button></td>
+    <td><button onclick="updateData(${i})" id="update-item">update</button></td>
     <td><button onclick="deleteItem(${i})" id= "delete-item">delete</button></td>
 </tr>
     `;
@@ -177,4 +221,107 @@ function deleteAll(){
     deletes.classList.add("hide")
     dataProduct = []
     readData()
+}
+
+function updateData(i){
+    title.value = dataProduct[i].title
+    price.value = dataProduct[i].price
+    ads.value = dataProduct[i].ads
+    taxes.value = dataProduct[i].taxes
+    discount.value = dataProduct[i].discount
+    // count.value = dataProduct[i].count
+    category.value = dataProduct[i].category
+    getPrice()
+    count.style.display = "none"
+    totalContainer.style.background = 'green'
+    submit.innerHTML = 'UPDATE'
+    submitButton = 'update'
+    tmp = i
+    scroll({
+        top:0,
+        behavior:"smooth"
+    })
+}
+
+
+// search function
+
+let searchMode = 'title'
+
+function getSearchMode(id){
+    readData()
+    searchs.value = ''
+    if(id == 's-title'){
+        searchMode = 'title'
+        searchs.placeholder = 'Search By Title'
+    }else{
+        searchMode = 'category'
+        searchs.placeholder = 'Search By Category'
+    }
+    searchs.focus()
+    console.log(searchMode)
+}
+
+
+function searchData(value){
+    let table = ''
+    if(searchMode == 'title'){
+        for(let i = 0 ; i < dataProduct.length ; i++){
+            if(dataProduct[i].title.includes(value.toLowerCase()) || dataProduct[i].title.includes(value.toUpperCase())){
+                table += `
+                <tr>
+                <td>${i+1}</td>
+                <td>${dataProduct[i].title}</td>
+                <td>${dataProduct[i].price}</td>
+                <td>${dataProduct[i].taxes}</td>
+                <td>${dataProduct[i].ads}</td>
+                <td>${dataProduct[i].discount}</td>
+                <td>${dataProduct[i].category}</td>
+                <td>${dataProduct[i].total}</td>
+                <td><button onclick="updateData(${i})" id="update-item">update</button></td>
+                <td><button onclick="deleteItem(${i})" id= "delete-item">delete</button></td>
+            </tr>
+                `;
+}
+}
+}else{
+    for(let i = 0 ; i < dataProduct.length ; i++){
+        if(dataProduct[i].category.includes(value.toLowerCase()) || dataProduct[i].category.includes(value.toUpperCase())){
+            table += `
+            <tr>
+            <td>${i+1}</td>
+            <td>${dataProduct[i].title}</td>
+            <td>${dataProduct[i].price}</td>
+            <td>${dataProduct[i].taxes}</td>
+            <td>${dataProduct[i].ads}</td>
+            <td>${dataProduct[i].discount}</td>
+            <td>${dataProduct[i].category}</td>
+            <td>${dataProduct[i].total}</td>
+            <td><button onclick="updateData(${i})" id="update-item">update</button></td>
+            <td><button onclick="deleteItem(${i})" id= "delete-item">delete</button></td>
+        </tr>
+            `;
+}
+}
+    }
+    tBody.innerHTML = table;
+
+}
+
+searchs.onblur = function(){
+    searchs.placeholder = 'Search'
+    readData()
+    searchs.value = ''
+}
+count.onblur = function(){
+    count.placeholder = 'Count'
+}
+title.onblur = function(){
+    title.placeholder = 'Title'
+}
+category.onblur = function(){
+    category.placeholder = 'Category'
+}
+price.onblur = function(){
+    price.placeholder = 'Price'
 }
